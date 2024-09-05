ee241e2e48a9c2ca27ae769b170d6595d01e79d1
Mirko <gerez.valenzuela@gmail.com>
2024-09-05 20:22:17 -0300

    entrega3

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ const questions = [
     ],
   },
   {
-    question: "¿Quién tiene mejór desarrollo de personaje?",
+    question: "¿Quién tiene mejor desarrollo de personaje?",
     answers: [
       { text: "Goku", correct: false },
       { text: "Saitama", correct: false },
@@ -35,24 +35,6 @@ const questions = [
     ],
   },
   {
-    question: "¿Cómo se llama el profesor de JS?",
-    answers: [
-      { text: "Roberto", correct: false },
-      { text: "Daniel", correct: true },
-      { text: "Lautaro", correct: false },
-      { text: "Juan", correct: false },
-    ],
-  },
-  {
-    question: "¿Cuál es el río más largo del mundo?",
-    answers: [
-      { text: "Nilo", correct: true },
-      { text: "Amazonas", correct: false },
-      { text: "Yangtsé", correct: false },
-      { text: "Mississippi", correct: false },
-    ],
-  },
-  {
     question: "¿Quién escribió 'Don Quijote de la Mancha'?",
     answers: [
       { text: "William Shakespeare", correct: false },
@@ -61,81 +43,55 @@ const questions = [
       { text: "Pablo Neruda", correct: false },
     ],
   },
-  {
-    question: "¿Qué elemento químico tiene el símbolo 'O'?",
-    answers: [
-      { text: "Oro", correct: false },
-      { text: "Oxígeno", correct: true },
-      { text: "Osmio", correct: false },
-      { text: "Oxalato", correct: false },
-    ],
-  },
-  {
-    question: "¿En qué continente se encuentra Egipto?",
-    answers: [
-      { text: "Asia", correct: false },
-      { text: "África", correct: true },
-      { text: "Europa", correct: false },
-      { text: "Oceanía", correct: false },
-    ],
-  },
-  {
-    question: "¿En qué año se lanzó League of legends?",
-    answers: [
-      { text: "2010", correct: false },
-      { text: "2008", correct: false },
-      { text: "2009", correct: true },
-      { text: "2011", correct: false },
-    ],
-  },
-  {
-    question: "¿Cómo se llama la última película de Jurassic World",
-    answers: [
-      { text: "Jurassic World Mundo Caído", correct: false },
-      { text: "Jurassic World ", correct: false },
-      { text: "Jurassic World 4", correct: false },
-      { text: "Jurassic World Dominion", correct: true },
-    ],
-  },
 ];
+
+localStorage.setItem('triviaQuestions', JSON.stringify(questions));
+
 startButton.addEventListener("click", startGame);
+
 function startGame() {
   startButton.classList.add("hide");
   questionContainer.classList.remove("hide");
-  shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+
+  shuffledQuestions = JSON.parse(localStorage.getItem('triviaQuestions')).sort(() => Math.random() - 0.5);
   currentQuestionIndex = 0;
   score = 0;
   setNextQuestion();
 }
+
 function setNextQuestion() {
   resetState();
   showQuestion(shuffledQuestions[currentQuestionIndex]);
 }
+
 function showQuestion(question) {
   questionElement.innerText = question.question;
   question.answers.forEach((answer) => {
     const button = document.createElement("button");
     button.innerText = answer.text;
     button.classList.add("btn");
+    button.addEventListener("click", selectAnswer);
     if (answer.correct) {
       button.dataset.correct = answer.correct;
     }
-    button.addEventListener("click", selectAnswer);
     answerButtonsElement.appendChild(button);
   });
 }
+
 function resetState() {
   while (answerButtonsElement.firstChild) {
     answerButtonsElement.removeChild(answerButtonsElement.firstChild);
   }
   resultElement.classList.add("hide");
 }
+
 function selectAnswer(e) {
   const selectedButton = e.target;
-  const correct = selectedButton.dataset.correct;
+  const correct = selectedButton.dataset.correct === 'true';
   if (correct) {
     score++;
   }
+  
   if (shuffledQuestions.length > currentQuestionIndex + 1) {
     currentQuestionIndex++;
     setNextQuestion();
@@ -143,10 +99,12 @@ function selectAnswer(e) {
     endGame();
   }
 }
+
 function endGame() {
   questionContainer.classList.add("hide");
   resultElement.classList.remove("hide");
-  resultElement.innerText = `Juego terminado. Tu puntaje es: ${score} de ${questions.length}`;
+  resultElement.innerText = `Juego terminado. Tu puntaje es: ${score} de ${shuffledQuestions.length}`;
   startButton.innerText = "Reiniciar Juego";
   startButton.classList.remove("hide");
+  startButton.addEventListener("click", startGame);
 }
